7e23959cf7fbe884191b39b3fdf60cc5464920ee
Sebastian <sebastianramostueros.srt@gmail.com>
2025-06-01 23:23:39 +0200

    header actualizado

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,18 +3,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('recetas.index') }}" class="flex items-center"> <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <span class="ms-3 text-lg font-semibold text-gray-800 dark:text-gray-200">RecetarIO</span>
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('recetas.index')" :active="request()->routeIs('dashboard')">
-                        {{ __('RecetarIO') }}
-                    </x-nav-link>
                 </div>
             </div>
 
